3f24925114906cd390780fe53abeefd350fec060
Jonah Shoemaker <jonah.shoemaker@gmail.com>
2018-05-02 15:33:29 -0700

    Pu-push it real good

--- transform2.py
+++ transform2.py
@@ -3,21 +3,21 @@ import matplotlib.pyplot as plt
 import scipy.fftpack
 
 
-Dt = 0.0001
-t = np.arange(0,1,Dt)
-x1 = np.sqrt(2) * 0.5 * -1j * (np.exp(1j*np.pi*t) - np.exp(-1j*np.pi*t))
-x2 = np.sqrt(2) * np.sin(2 * np.pi * t)
-x3 = np.sqrt(2) * np.sin(3 * np.pi * t)
+Dt = 0.001
+t = np.arange(-10,10,Dt)
+x1 = np.exp(-t**2)
+x2 = t*np.exp(-t**2)
+x3 = (t**2-1)*np.exp(-t**2)
 
 def wavefunction1(x):
-    return np.sqrt(2) * 0.5 * -1j * (np.exp(1j*np.pi*x) - np.exp(-1j*np.pi*x))
+    return np.exp(-x**2)
 def wavefunction2(x):
-    return np.sqrt(2) * np.sin(2 * np.pi * x)
+    return x*np.exp(-x**2)
 def wavefunction3(x):
-    return np.sqrt(2) * np.sin(3 * np.pi * x)
+    return (x**2-1)*np.exp(-x**2)
 
 
-def FT(f1,g1,xmin=-10,xmax=10,kmin=-10,kmax=10,nx=20000,nk=20000):
+def FT(f1,g1,xmin=-10,xmax=10):
 
     y = np.fft.fftshift(np.fft.fft(g1))
     m = Dt*np.abs(y)
@@ -44,23 +44,23 @@ def FT(f1,g1,xmin=-10,xmax=10,kmin=-10,kmax=10,nx=20000,nk=20000):
     phi = np.abs(phi)
     return k, phi, m, f2
 
-k, yalg1, m1, f2 = FT(wavefunction1,x1,xmin=0,xmax=1)
-k, yalg2, m2, f2 = FT(wavefunction2,x2,xmin=0,xmax=1)
-k, yalg3, m3, f2 = FT(wavefunction3,x3,xmin=0,xmax=1)
-ycontrol1 = np.abs(-(np.sqrt(2) * np.pi * (1 - np.exp(-1j*k)))/(k**2 - np.pi**2))
-ycontrol2 = np.abs(-(np.sqrt(2) * np.pi * (2 - 2*np.exp(-1j*k)))/(k**2 - 4 * np.pi**2))
-ycontrol3 = np.abs(-(3*np.sqrt(2) * np.pi * (1 + np.exp(-1j*k)))/(k**2 - 9 * np.pi**2))
+k, yalg1, m1, f2 = FT(wavefunction1,x1,xmin=-10,xmax=10)
+k, yalg2, m2, f2 = FT(wavefunction2,x2,xmin=-10,xmax=10)
+k, yalg3, m3, f2 = FT(wavefunction3,x3,xmin=-10,xmax=10)
+ycontrol1 = np.abs(np.sqrt(np.pi) * np.exp(-k**2 / 4))
+ycontrol2 = np.abs(-0.5*1j*k*np.sqrt(np.pi) * np.exp(-k**2 / 4))
+ycontrol3 = np.abs(-0.25*np.sqrt(np.pi) *(k**2 +2) * np.exp(-k**2 / 4))
 
 plt.subplot(2,1,1)
 plt.plot(k,ycontrol1, '-', color='rebeccapurple',label='Ground State')
 plt.plot(k,ycontrol2, '-', color='red',label='First Excited State')
 plt.plot(k,ycontrol3, '-', color='blue',label='Second Excited State')
 plt.title('Calculated by Hand')
-plt.xlim(-20,20)
-plt.ylim(-2,2)
+plt.xlim(-10,10)
+plt.ylim(0,2)
 plt.xlabel('k')
 plt.ylabel('Phi(k)')
-plt.title('K-Space Transforms for the Infinite Square Well')
+plt.title('K-Space Transforms for the Harmonic Oscillator')
 plt.legend()
 plt.subplot(2,1,2)
 plt.plot(f2,m1,'o',color='cornflowerblue',label='FFT Ground State')
@@ -69,8 +69,8 @@ plt.plot(f2,m2,'o',color='green',label='FFT First Excited State')
 plt.plot(k,yalg2,'-',color='orchid',label='Algorithm First Excited State')
 plt.plot(f2,m3,'o',color='orange',label='FFT Second Excited State')
 plt.plot(k,yalg3,'-',color='black',label='Algorithm Second Excited State')
-plt.xlim(-20,20)
-plt.ylim(-2,2)
+plt.xlim(-10,10)
+plt.ylim(0,2)
 plt.xlabel('k')
 plt.ylabel('Phi(k)')
 plt.title('FFT and Algorithm Outputs')
